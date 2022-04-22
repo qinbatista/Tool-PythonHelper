f0477310635f2418daaf3b5a114d2f04da6a3e18
MacStudio <qin>
2022-04-22 00:25:54 -0700

    update new test

--- iptest.py
+++ iptest.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 import os
+from socket import *
 import sys
 if __name__ == "__main__":
-	if len(sys.argv)>=2:
-		os.system('curl http://www.cip.cc/'+sys.argv[1])
-	else:
-		os.system('curl http://www.cip.cc')
+    if len(sys.argv)>=2:
+        if sys.argv[1].find(".")!=-1:
+            this_ip = gethostbyname(sys.argv[1])
+        else:
+            this_ip = sys.argv[1]
+    if len(sys.argv)>=2:
+        os.system('curl http://www.cip.cc/'+this_ip)
+    else:
+        os.system('curl http://www.cip.cc')
